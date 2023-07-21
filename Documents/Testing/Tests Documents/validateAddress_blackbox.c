60230153d04a5ca5378313c31e98ef263e32c3df
WAISUN0813 <137556160+WAISUN0813@users.noreply.github.com>
2023-07-21 16:03:42 -0400

    Update and rename validateAddress.c to validateAddress_blackbox.c

--- Documents/Testing/Tests Documents/validateAddress_blackbox.c
+++ Documents/Testing/Tests Documents/validateAddress_blackbox.c
@@ -3,21 +3,21 @@
 
 void test_validateAddress() {
     // Test with valid coordinates
-    assert(validateAddress(10, 'L') == true);
+    assert(validateAddress(10, 'L') == 1);
 
     // Test with coordinates at the lower limit
-    assert(validateAddress(1, 'A') == true);
+    assert(validateAddress(1, 'A') == 1);
 
     // Test with coordinates at the upper limit
-    assert(validateAddress(25, 'Y') == true);
+    assert(validateAddress(25, 'Y') == 1);
 
     // Test with coordinates below the valid range
-    assert(validateAddress(0, 'A') == false);
-    assert(validateAddress(10, 'Z') == false);
+    assert(validateAddress(0, 'A') == 0);
+    assert(validateAddress(10, 'Z') == 0);
 
     // Test with coordinates above the valid range
-    assert(validateAddress(26, 'A') == false);
-    assert(validateAddress(10, 'Z') == false);
+    assert(validateAddress(26, 'A') == 0);
+    assert(validateAddress(10, 'Z') == 0);
 
     printf("All tests passed.\n");
 }
