3f4ea5a4d4342cf5188f42a0a6f077acc409c713
WAISUN0813 <137556160+WAISUN0813@users.noreply.github.com>
2023-07-23 15:46:14 -0400

    Add files via upload

---
+++ Documents/Testing/Tests Documents/validateSize_whitebox_blackbox.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+int validateSize(double size) {
+    // Assume the implementation of validateSize() exists here.
+    // You can replace this with your actual implementation.
+    // For demonstration purposes, I'll just return 1 for valid sizes and 0 for invalid sizes.
+    if (size == 0.25 || size == 0.5 || size == 1.0)
+        return 1;
+    else
+        return 0;
+}
+
+void test_validateSizeBlackBox() {
+    // Test with a valid size
+    printf("Test 1: %d\n", validateSize(0.25));
+
+    // Test with another valid size
+    printf("Test 2: %d\n", validateSize(0.5));
+
+    // Test with yet another valid size
+    printf("Test 3: %d\n", validateSize(1.0));
+
+    // Test with an invalid size
+    printf("Test 4: %d\n", validateSize(0.75));
+
+    printf("Black box tests passed.\n");
+}
+
+void testValidateSizeWhiteBox() {
+    // Test 1: Test with values equal to the expected ones
+    printf("Test 1: %d\n", validateSize(0.25));
+    printf("Test 2: %d\n", validateSize(0.5));
+    printf("Test 3: %d\n", validateSize(1));
+
+    // Test 2: Test with values just below the expected ones
+    printf("Test 4: %d\n", validateSize(0.24));
+    printf("Test 5: %d\n", validateSize(0.49));
+    printf("Test 6: %d\n", validateSize(0.99));
+
+    // Test 3: Test with values just above the expected ones
+    printf("Test 7: %d\n", validateSize(0.26));
+    printf("Test 8: %d\n", validateSize(0.51));
+    printf("Test 9: %d\n", validateSize(1.01));
+
+    // Test 4: Test with zero
+    printf("Test 10: %d\n", validateSize(0.0));
+
+    // Test 5: Test with negative values
+    printf("Test 11: %d\n", validateSize(-0.25));
+    printf("Test 12: %d\n", validateSize(-0.5));
+    printf("Test 13: %d\n", validateSize(-1));
+
+    printf("White box tests passed.\n");
+}
+
+int main() {
+    test_validateSizeBlackBox();
+    testValidateSizeWhiteBox();
+    return 0;
+}
