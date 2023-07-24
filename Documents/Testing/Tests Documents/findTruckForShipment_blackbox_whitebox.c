7b848bd8e469acc48d6992240d848bc8f6b6353f
xinyangma01 <130402682+xinyangma01@users.noreply.github.com>
2023-07-24 14:15:09 -0400

    Update findTruckForShipment_blackbox_whitebox.c

--- Documents/Testing/Tests Documents/findTruckForShipment_blackbox_whitebox.c
+++ Documents/Testing/Tests Documents/findTruckForShipment_blackbox_whitebox.c
@@ -39,7 +39,7 @@ void runwhiteboxTests() {
        {1000, 36, 'Y'}, // Truck 2
     };
     struct Shipment shipment3 = { 10, 1, {7, 'S'} };
-    printf("Test 3: Should return 0\n");
+    printf("Test 3: Should return 1\n");
     int result3 = findTruckForShipment(&deliveryMap, Truck3, shipment3);
     printf("Result: %d\n\n", result3);
 
@@ -54,9 +54,9 @@ void runwhiteboxTests() {
 
     // Test 5: Same distance, different availability
     struct Truck Truck5[NUMTRUCKS] = {
-       {700, 35, 'B'}, {800, 35, 'G'}, {900, 35, 'Y'},
+       {700, 35, 'B'}, {700, 34, 'G'}, {700, 33, 'Y'},
     };
-    struct Shipment shipment5 = { 10, 1, {3, 'c'} };
+    struct Shipment shipment5 = { 10, 1, {3, 'C'} };
     printf("Test 5: Should return 0\n");
     int result5 = findTruckForShipment(&deliveryMap, Truck5, shipment5);
     printf("Result: %d\n\n", result5);
@@ -65,7 +65,7 @@ void runwhiteboxTests() {
     struct Truck Truck6[NUMTRUCKS] = {
        {400, 35, 'B'}, {700, 34, 'G'}, {700, 33, 'Y'},
     };
-    struct Shipment shipment6 = { 500, 1, {14, 'u'} };
+    struct Shipment shipment6 = { 500, 1, {14, 'U'} };
     printf("Test 6: Should return 1\n");
     int result6 = findTruckForShipment(&deliveryMap, Truck6, shipment6);
     printf("Result: %d\n\n", result6);
@@ -77,13 +77,14 @@ void runwhiteboxTests() {
 void runblackboxTests() {
     struct Map deliveryMap;
     
+    //Test 1: All full
     struct Truck Truck1[NUMTRUCKS] = {
     {0, 3, 'B'}, // Truck 0
     {0, 4, 'G'},  // Truck 1
     {0, 3, 'Y'}, // Truck 2
     };
     struct Shipment shipment1 = { 500, 1, {14, 'U'} };
-    printf("Test 1: Should return 0\n");
+    printf("Test 1: Should return -1\n");
     int result1 = findTruckForShipment(&deliveryMap, Truck1, shipment1);
     printf("Result: %d\n\n", result1);
 
@@ -119,7 +120,7 @@ void runblackboxTests() {
        {500, 0.25, 'Y'}, // Truck 2
     };
     struct Shipment shipment4 = { 500, 1, {13, 'U'} };
-    printf("Test 4: Should return 0\n");
+    printf("Test 4: Should return -1\n");
     int result4 = findTruckForShipment(&deliveryMap, Truck4, shipment4);
     printf("Result: %d\n\n", result4);
 
@@ -130,7 +131,7 @@ void runblackboxTests() {
        {100, 1, 'Y'}, // Truck 2
     };
     struct Shipment shipment5 = { 500, 1, {13, 'U'} };
-    printf("Test 5: Should return 0\n");
+    printf("Test 5: Should return -1\n");
     int result5 = findTruckForShipment(&deliveryMap, Truck5, shipment5);
     printf("Result: %d\n\n", result5);
 
