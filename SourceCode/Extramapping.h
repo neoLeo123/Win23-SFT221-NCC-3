ddca03c84f83887d788f7cfccc323aa93a767b94
neoLeo123 <113144967+neoLeo123@users.noreply.github.com>
2023-07-23 21:57:52 -0400

    Add files via upload

---
+++ SourceCode/Extramapping.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#define MAX_WEIGHT 1000
+#define MAX_SIZE 36
+
+#include "mapping.h"
+
+struct Truck
+{
+	int availWeight;
+	int availSize;
+	char route;
+};
+
+struct Shipment {
+	int weight;
+	int size;
+	struct Point dest;
+};
+
+
